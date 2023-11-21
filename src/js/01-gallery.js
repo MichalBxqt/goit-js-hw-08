207fdc1829c924864ad3f1c7a50fa22663f8abac
Michał Błaszczyk <michal.blaszczyk00@gmail.com>
2023-11-21 17:43:22 +0100

    simplebox task1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,25 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+// Opisany w dokumentacji
+import SimpleLightbox from "simplelightbox";
+// Dodatkowy import stylów
+import "simplelightbox/dist/simple-lightbox.min.css" ;
 
-console.log(galleryItems);
+
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+const galleryContainer = document.querySelector(".gallery");
+
+for (const imgList of galleryItems) {
+  const imgEl =
+`<li class="gallery__item">
+<a class="gallery__link" href="${imgList.original}">
+  <img class="gallery__image" src="${imgList.preview}" alt="${imgList.description}" />
+</a>
+</li>
+`;
+  galleryContainer.insertAdjacentHTML("beforeend", imgEl);
+}
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
